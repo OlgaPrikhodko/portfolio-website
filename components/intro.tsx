686c0bedc20e3e090a683fe708370b16eb20ade7
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-01-27 12:58:05 +0200

    :star: Intro: add text

--- components/intro.tsx
+++ components/intro.tsx
@@ -5,7 +5,7 @@ import { motion } from "framer-motion";
 
 export default function Intro() {
   return (
-    <section>
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
         <div className="relative">
           <motion.div
@@ -39,6 +39,22 @@ export default function Intro() {
           </motion.span>
         </div>
       </div>
+
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5]"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Olha</span>, an experienced
+        <span className="font-bold"> full-stack developer </span>
+        focused on front-end development, passionate about{" "}
+        <span className="font-italic">
+          creating high-performance, user-centric products
+        </span>
+        . Proficient in <span className="underline">Vue and Python</span>, with
+        recent upskilling in{" "}
+        <span className="underline">TypeScript, and React</span>.
+      </motion.p>
     </section>
   );
 }
