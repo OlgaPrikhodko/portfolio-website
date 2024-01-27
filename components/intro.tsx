686fd3f61407597efd143828851195b1281c3ef9
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-01-27 14:09:47 +0200

    :star: Intro: add buttons panel

--- components/intro.tsx
+++ components/intro.tsx
@@ -2,6 +2,10 @@
 import MePhoto from "@/public/me.jpg";
 import Image from "next/image";
 import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
 
 export default function Intro() {
   return (
@@ -55,6 +59,45 @@ export default function Intro() {
         recent upskilling in{" "}
         <span className="underline">TypeScript, and React</span>.
       </motion.p>
+
+      <motion.div
+        className="px-4 text-lg font-medium flex flex-col items-center justify-center gap-2 sm:flex-row"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.1 }}
+      >
+        <Link
+          href="#contact"
+          className="group bg-gray-900 text-white py-3 px-7 flex gap-2 items-center rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+        >
+          Contact me here{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
+        </Link>
+
+        <a
+          href="/CV.pdf"
+          download
+          className="group bg-white py-3 px-7 flex gap-2 items-center rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          href="https://www.linkedin.com/in/olga-prikhodko-a347688b/"
+          target="_blank"
+          className="bg-white p-4 text-gray-700 flex gap-2 items-center rounded-full outline-none focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          <BsLinkedin />
+        </a>
+        <a
+          href="https://github.com/OlgaPrikhodko"
+          target="_blank"
+          className="bg-white p-4 text-gray-700 text-[1.35rem] flex gap-2 items-center rounded-full outline-none focus:scale-[1.15] hover:text-gray-950 hover:scale-[1.15] active:scale-105 transition cursor-pointer border border-black/10"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
     </section>
   );
 }
