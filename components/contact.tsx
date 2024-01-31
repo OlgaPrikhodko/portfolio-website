33669937118ee55fd85b0fc76364607d1c0dc5b5
Olga Prykhodko <helga.prikhodko@gmail.com>
2024-01-31 13:23:16 +0200

    :star: add sendEmail function with simple validation and error catching

--- components/contact.tsx
+++ components/contact.tsx
@@ -4,6 +4,7 @@ import { FaPaperPlane } from "react-icons/fa";
 import SectionHeading from "./section-heading";
 import { motion } from "framer-motion";
 import { useSectionInView } from "@/lib/hooks";
+import { sendEmail } from "@/actions/sendEmail";
 
 export default function Contact() {
   const { ref } = useSectionInView("Contact");
@@ -28,15 +29,26 @@ export default function Contact() {
         or through this form
       </p>
 
-      <form className="mt-10 flex flex-col">
+      <form
+        className="mt-10 flex flex-col"
+        action={async (formData) => {
+          await sendEmail(formData);
+        }}
+      >
         <input
           className="h-14 px-4 rounded-lg borderBlack"
+          name="senderEmail"
           type="email"
+          required
+          maxLength={500}
           placeholder="Your email"
         />
         <textarea
           className="h-52 my-3 rounded borderBlack p-4"
+          name="message"
           placeholder="Your message"
+          required
+          maxLength={5000}
         ></textarea>
         <button
           className="group flex items-center justify-center gap-2 h-[3rem] w-[8rem] bg-gray-900 text-white rounded-full outline-none transition-all focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105"
